08ad6425066c8b8b7521ea70f3dd68835940a5f2
walbertjr12 <58478123+walbertjr12@users.noreply.github.com>
2021-01-27 11:07:35 -0600

    Proyecto Strapi

---
+++ strapi_rest/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a7108b60cc6d87319d8d6f89c0c92101'),
+    },
+  },
+});
---
+++ strapi_rest/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3760b06e-cfd5-46a8-91ce-622997b5053e'
+};
